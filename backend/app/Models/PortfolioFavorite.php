115c571e8745bbda3c579b2e241397bd87523275
Azzam-Olaya <azzamolaya00@gmail.com>
2026-04-25 23:01:17 +0100

    feat: add relationships for portfolio and user in PortfolioFavorite model

--- backend/app/Models/PortfolioFavorite.php
+++ backend/app/Models/PortfolioFavorite.php
@@ -3,4 +3,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 class PortfolioFavorite extends Model {
     protected $fillable = ['portfolio_id', 'user_id'];
+
+    public function portfolio() { return $this->belongsTo(Portfolio::class); }
+    public function user() { return $this->belongsTo(User::class); }
 }
